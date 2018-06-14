2fa0db3c1d5e8b96ac9f2c0aeb3f203c66cd9671
Mark Thorley <mark.thorley@designtech.io>
2018-06-14 16:07:23 +0100

    import project

---
+++ WindowViewModel.cs
@@ -0,0 +1,258 @@
+﻿using Dynamo.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Extensions;
+using Dynamo.Graph.Nodes;
+
+namespace designtechViewExtension
+{
+    class WindowViewModel : NotificationObject, IDisposable
+    {
+
+        #region Fields
+        private string graphFileName;
+        private string graphFilePath;
+        private string graphDescription;
+        private string graphLastSaved;
+        private string activeNodeCount;
+        private string activeWireCount;
+        private string errorNodeTypes;
+        private ReadyParams readyParams;
+
+        #endregion
+
+        #region Property
+        // Displays the graph file name
+        public string GraphFileName
+        {
+            get
+            {
+                graphFileName = getGraphFileName();
+                return graphFileName;
+            }
+        }
+
+        // Displays the graph file name
+        public string GraphFilePath
+        {
+            get
+            {
+                graphFilePath = getGraphFilePath();
+                return graphFilePath;
+            }
+        }
+
+        // Displays the graph file name
+        public string GraphDescription
+        {
+            get
+            {
+                graphDescription = getGraphDescription();
+                return graphDescription;
+            }
+        }
+
+        // Displays the graph file name
+        public string GraphLastSaved
+        {
+            get
+            {
+                graphLastSaved = getGraphLastSaved();
+                return graphLastSaved;
+            }
+        }
+
+        //Displays the count of the nodes in the workspace
+        public string ActiveNodeCount
+        {
+            get
+            {
+                activeNodeCount = getTotalNodeCount();
+                return activeNodeCount;
+            }
+        }
+
+        //Displays the count of the wires in the workspace
+        public string ActiveWireCount
+        {
+            get
+            {
+                activeWireCount = getTotalWireCount();
+                return activeWireCount;
+            }
+        }
+
+        // Displays error nodes in the workspace
+        public string ErrorNodeTypes
+        {
+            get
+            {
+                errorNodeTypes = getErrorNodeTypes();
+                return errorNodeTypes;
+            }
+        }
+
+        #endregion
+
+        #region Functions
+        // Helper function that retrieves the name of the graph
+        public string getGraphFileName()
+        {
+            Dynamo.Graph.Workspaces.IWorkspaceModel curWS = readyParams.CurrentWorkspaceModel;
+            string f = curWS.FileName;
+
+            if (f == "")
+            {
+                return "File has not been saved yet";
+            }
+            int ind = f.LastIndexOf("\\");
+            string fN = f.Remove(0, (ind + 1));
+            return fN;
+        }
+
+        // Helper function that retrieves the path of the graph
+        public string getGraphFilePath()
+        {
+            Dynamo.Graph.Workspaces.IWorkspaceModel curWS = readyParams.CurrentWorkspaceModel;
+            string f = curWS.FileName;
+
+            if (f == "")
+            {
+                return "File has not been saved yet";
+            }
+            int ind = f.LastIndexOf("\\");
+            string fN = f.Remove(0, (ind + 1));
+            string fP = f.Remove((ind + 1),fN.Length);
+            return fP;
+        }
+
+        // Helper function that retrieves the description of the graph
+        public string getGraphDescription()
+        {
+            Dynamo.Graph.Workspaces.IWorkspaceModel curWS = readyParams.CurrentWorkspaceModel;
+            string f = curWS.Description;
+
+            if (f == "")
+            {
+                return "File has no description";
+            }
+            return f;
+        }
+
+        // Helper function that retrieves the description of the graph
+        public string getGraphLastSaved()
+        {
+            Dynamo.Graph.Workspaces.IWorkspaceModel curWS = readyParams.CurrentWorkspaceModel;
+            Dynamo.Graph.Workspaces.WorkspaceModel wS = curWS as Dynamo.Graph.Workspaces.WorkspaceModel;
+            string f = wS.LastSaved.ToString();
+
+            if (f == "")
+            {
+                return "File has not been saved";
+            }
+            return f;
+        }
+
+        // Helper function that counts the number of nodes
+        public string getTotalNodeCount()
+        {
+
+            int count = 0;
+            foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
+            {
+                count++;
+            }
+            string output = count.ToString();
+            return output;
+        }
+
+        // Helper function that counts the number of wires
+        public string getTotalWireCount()
+        {
+
+            int count = 0;
+            foreach (Dynamo.Graph.Connectors.ConnectorModel node in readyParams.CurrentWorkspaceModel.Connectors)
+            {
+                count++;
+            }
+            string output = count.ToString();
+            return output;
+        }
+
+        // Helper function that builds string of error nodes
+        public string getErrorNodeTypes()
+        {
+            string output = "";
+            foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
+            {
+                if (node.State.ToString() != "Active" && node.Name != "Watch")
+                {
+                    string nickName = node.Name;
+                    output += nickName + "\n";
+                }
+
+            }
+
+            return output;
+
+        }
+
+        #endregion
+
+        #region ReadyParams
+        public WindowViewModel(ReadyParams p)
+        {
+            readyParams = p;
+            p.CurrentWorkspaceChanged += CurrentWorkspaceModel_GraphSaved;
+            p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodeCount;
+            p.CurrentWorkspaceModel.NodeRemoved += CurrentWorkspaceModel_NodeCount;
+            p.CurrentWorkspaceModel.ConnectorAdded += CurrentWorkspaceModel_WireCount;
+            p.CurrentWorkspaceModel.ConnectorDeleted += CurrentWorkspaceModel_WireCount;
+            p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodesChanged;
+            p.CurrentWorkspaceModel.NodeRemoved += CurrentWorkspaceModel_NodesChanged;
+        }
+
+        #endregion
+
+        #region ChangeProperty
+
+        private void CurrentWorkspaceModel_GraphSaved(Dynamo.Graph.Workspaces.IWorkspaceModel obj)
+        {
+            RaisePropertyChanged("GraphLastSaved");
+        }
+
+        private void CurrentWorkspaceModel_NodeCount(NodeModel obj)
+        {
+            RaisePropertyChanged("ActiveNodeCount");
+        }
+
+        private void CurrentWorkspaceModel_WireCount(Dynamo.Graph.Connectors.ConnectorModel obj)
+        {
+            RaisePropertyChanged("ActiveWireCount");
+        }
+
+        private void CurrentWorkspaceModel_NodesChanged(NodeModel obj)
+        {
+            RaisePropertyChanged("ErrorNodeTypes");
+        }
+
+
+        #endregion
+
+        #region Dispose Methods
+        public void Dispose()
+        {
+            readyParams.CurrentWorkspaceChanged += CurrentWorkspaceModel_GraphSaved;
+            readyParams.CurrentWorkspaceModel.NodeAdded -= CurrentWorkspaceModel_NodeCount;
+            readyParams.CurrentWorkspaceModel.NodeRemoved -= CurrentWorkspaceModel_NodeCount;
+            readyParams.CurrentWorkspaceModel.ConnectorAdded -= CurrentWorkspaceModel_WireCount;
+            readyParams.CurrentWorkspaceModel.ConnectorDeleted -= CurrentWorkspaceModel_WireCount;
+            readyParams.CurrentWorkspaceModel.NodeAdded -= CurrentWorkspaceModel_NodesChanged;
+            readyParams.CurrentWorkspaceModel.NodeRemoved -= CurrentWorkspaceModel_NodesChanged;
+        }
+        #endregion
+    }
+}
---
+++ designtechViewExtension.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Wpf.Extensions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace designtechViewExtension
+{
+    public class designtechViewExtension : IViewExtension
+    {
+        private MenuItem sampleMenuItem;
+
+        public void Dispose()
+        {
+        }
+
+        public void Startup(ViewStartupParams p)
+        {
+        }
+
+        public void Loaded(ViewLoadedParams p)
+        {
+            // Save a reference to your loaded parameters.
+            // You'll need these later when you want to use
+            // the supplied workspaces
+
+            sampleMenuItem = new MenuItem { Header = "designtech Graph Diagnostics" };
+            sampleMenuItem.Click += (sender, args) =>
+            {
+                var viewModel = new WindowViewModel(p);
+                var window = new designtechWindow
+                {
+                    // Set the data context for the main grid in the window.
+                    MainGrid = { DataContext = viewModel },
+
+                    // Set the owner of the window to the Dynamo window.
+                    Owner = p.DynamoWindow
+                };
+
+                window.Left = window.Owner.Left + 400;
+                window.Top = window.Owner.Top + 200;
+
+                // Show a modeless window.
+                window.Show();
+            };
+            p.AddMenuItem(MenuBarType.View, sampleMenuItem);
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public string UniqueId
+        {
+            get
+            {
+                return Guid.NewGuid().ToString();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "designtech View Extension";
+            }
+        }
+
+    }
+}
+
