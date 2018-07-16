bfb990b374a45c534806beac9b42e25bc0a31d3e
Mark Thorley <markt@DESKTOP-5PMK1JF>
2018-07-16 12:18:13 +0100

    added template for favourite nodes

--- designtechViewExtension.cs
+++ designtechViewExtension.cs
@@ -15,6 +15,7 @@ namespace designtechViewExtension
         private MenuItem designtechMenuItem;
         private MenuItem designtechAboutMenuItem;
         private MenuItem designtechGraphInformationMenuItem;
+        private MenuItem designtechFavouriteNodesMenuItem;
         private MenuItem designtechLiveWatchMenuItem;
 
         public void Dispose()
@@ -44,6 +45,26 @@ namespace designtechViewExtension
                 window.Show();
             };
             designtechMenuItem.Items.Add(designtechAboutMenuItem);
+            #endregion
+
+            #region Favourite Nodes
+            designtechFavouriteNodesMenuItem = new MenuItem { Header = "Favourite Nodes" };
+            designtechFavouriteNodesMenuItem.Click += (sender, args) =>
+            {
+                var viewModel = new FavouriteNodesViewModel(p);
+                var window = new FavouriteNodesWindow
+                {
+                    // Set the data context for the main grid in the window.
+                    MainGrid = { DataContext = viewModel },
+
+                    // Set the owner of the window to the Dynamo window.
+                    Owner = p.DynamoWindow
+                };
+                window.Left = window.Owner.Left + 400;
+                window.Top = window.Owner.Top + 200;
+                window.Show();
+            };
+            designtechMenuItem.Items.Add(designtechFavouriteNodesMenuItem);
             #endregion
 
             #region Graph Information
