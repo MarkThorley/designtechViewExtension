768e6e18fb3249fa141d83bec849d08c985ae8f2
Mark Thorley <markt@DESKTOP-5PMK1JF>
2018-07-23 21:59:38 +0100

    fixes to UI of graph information

--- GraphInformation/GraphInformationViewModel.cs
+++ GraphInformation/GraphInformationViewModel.cs
@@ -222,7 +222,7 @@ namespace designtechViewExtension
             List<nodeData> output = new List<nodeData>();
             foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
             {
-                if (node.State.ToString() == "Warning" && node.Name != "Watch")
+                if (node.State.ToString() == "Warning" && node.Name != "Watch" || node.State.ToString() == )
                 {
                     output.Add(new nodeData()
                     {
