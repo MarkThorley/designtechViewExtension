104b730dd51b728eaa3c0bdb0d958ac52048c975
Mark Thorley <markt@DESKTOP-5PMK1JF>
2018-10-11 13:38:06 +0100

    cleanup

--- ErrorNodes/ErrorNodesViewModel.cs
+++ ErrorNodes/ErrorNodesViewModel.cs
@@ -64,7 +64,6 @@ namespace designtechViewExtension
             foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
             {
                 if (node.State.ToString() == "Warning" || node.State.ToString() == "Dead" || node.State.ToString() == "Error" || node.State.ToString() == "PersistentWarning" || node.State.ToString() == "AstBuildBroken" && node.Name != "Watch")
-                //if (node.State.ToString() == "Warning" && node.Name != "Watch")
                 {
                     output.Add(new nodeData()
                     {
